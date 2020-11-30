f62945330947da8b3e7e4998401ff1e5abd73ab4
OneFoggyScreen <69231466+OneFoggyScreen@users.noreply.github.com>
2020-11-29 18:58:04 -0800

    Quality of life :D (fixed)

--- Guessing-Game.py
+++ Guessing-Game.py
@@ -1,21 +1,26 @@
-import random 
-import sys
-mn = 0
-mx = 15
-targetvalue = random.randint(mn, mx)
-rounds = int(input("How many rounds do you want?"))
+import random
+import time
+
+MN = 0
+MX = 15
+TARGETVALUE = random.randint(MN, MX)
+ROUNDS = int(input("How many rounds do you want? "))
 
 while True:
-    print(f"Guess the number! you have {str(rounds)} rounds")
-    print(f"The number is inbetween {str(mn)} {str(mx)}")
-    guess = input("")
-    if guess == targetvalue:
+
+    print(f"Guess the number! You have {str(ROUNDS)} rounds left ")
+    print(f"The number is inbetween {str(MN)} {str(MX)}")
+    GUESS = input("")
+
+    if str(GUESS) == str(TARGETVALUE):
         print("Congradulations you won!")
+        time.sleep(2)
         break
     else:
-        print("That was incorrect dumbass")
-        rounds = rounds - 1
-    if rounds <= 0:
-        print("You lost noo chooiche for you")
+        print("That was incorrect, dumbass")
+        ROUNDS -= 1
+    if ROUNDS <= 0:
+        print("You lost, no chooiche for you")
+        time.sleep(2)
         break
 
